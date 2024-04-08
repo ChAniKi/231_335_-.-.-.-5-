1f745fffb4cbf320fc9cbc7d521fce44043c2d11
AlexS <AlexS@ALEX>
2024-04-08 11:27:04 +0300

    Добавьте файлы проекта.

---
+++ 231_335_Шматов.А.М.(5)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _231_335_Шматов.А.М._5_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
